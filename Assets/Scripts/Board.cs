08a834f8ca3854f9e64b497a26945b9509d98b54
Frank Alfano <q24xboss@gmail.com>
2022-05-22 19:42:53 -0400

    Added piece moving, rotating, and placing

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour {
+	[SerializeField] private GameObject[ ] pieces;
+	[Space]
 	[SerializeField] private SpriteRenderer spriteRenderer;
 	[Space]
-	[SerializeField] [Range(10, 30)] private int boardWidth = 20;
-	[SerializeField] [Range(10, 30)] private int boardHeight = 20;
+	[SerializeField] [Range(10, 40)] public int BoardWidth = 20;
+	[SerializeField] [Range(10, 40)] public int BoardHeight = 20;
+	[SerializeField] [Range(2, 10)] private int topSpawnGap = 4;
 
-	private GameObject[ , ] board;
+	private Transform[ , ] board;
 
 #if UNITY_EDITOR
 	protected void OnValidate ( ) => EditorApplication.delayCall += _OnValidate;
 #endif
 	protected void _OnValidate ( ) {
 #if UNITY_EDITOR
-		// This is used to suppress warnings that Unity oh so kindy throws
 		EditorApplication.delayCall -= _OnValidate;
 		if (this == null) {
 			return;
@@ -28,13 +30,62 @@ public class Board : MonoBehaviour {
 		spriteRenderer = GetComponent<SpriteRenderer>( );
 
 		// Set the board size and position so the bottom left corner is at (0, 0)
-		spriteRenderer.size = new Vector2(boardWidth, boardHeight);
-		float positionX = (boardWidth / 2) - (boardWidth % 2 == 0 ? 0.5f : 0);
-		float positionY = (boardHeight / 2) - (boardHeight % 2 == 0 ? 0.5f : 0);
+		spriteRenderer.size = new Vector2(BoardWidth, BoardHeight);
+		float positionX = (BoardWidth / 2) - (BoardWidth % 2 == 0 ? 0.5f : 0);
+		float positionY = (BoardHeight / 2) - (BoardHeight % 2 == 0 ? 0.5f : 0);
 		transform.position = new Vector3(positionX, positionY);
 
 		// Set the camera orthographic size and position so it fits the entire board
-		Camera.main.orthographicSize = boardHeight / 2;
+		Camera.main.orthographicSize = BoardHeight / 2;
 		Camera.main.transform.position = new Vector3(positionX, positionY, Camera.main.transform.position.z);
+	}
+
+	private void Awake ( ) {
+		board = new Transform[BoardWidth, BoardHeight];
+	}
+
+	private void Start ( ) {
+		SpawnRandomPiece( );
+	}
+
+	public void SpawnRandomPiece ( ) {
+		Vector3 spawnPosition = new Vector3((BoardWidth / 2) - 0.5f, BoardHeight - topSpawnGap - 0.5f);
+		Instantiate(pieces[Random.Range(0, pieces.Length)], spawnPosition, Quaternion.identity);
+	}
+
+	public bool IsMoveInBounds (Piece piece, Vector3 direction) {
+		foreach (Transform tile in piece.transform) {
+			if (!IsInBounds(Vector3Int.RoundToInt(tile.transform.position + direction))) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public bool IsRotationInBounds (Piece piece, float degRotation) {
+		foreach (Transform tile in piece.transform) {
+			Vector3 newPosition = Quaternion.Euler(0, 0, degRotation) * (tile.position - piece.transform.position) + piece.transform.position;
+
+			if (!IsInBounds(Vector3Int.RoundToInt(newPosition))) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private bool IsInBounds (Vector3 position) {
+		bool isInBounds = (position.x >= 0 && position.x < BoardWidth && position.y >= 0 && position.y < BoardHeight);
+		return (isInBounds && board[Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y)] == null);
+	}
+
+	public void AddPieceToBoard (Piece piece) {
+		foreach (Transform tile in piece.transform) {
+			int x = Mathf.RoundToInt(tile.transform.position.x);
+			int y = Mathf.RoundToInt(tile.transform.position.y);
+
+			board[x, y] = tile;
+		}
 	}
 }
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+	[SerializeField] private Board board;
+	[Space]
+	[SerializeField] [Min(0.001f)] private float fallTime;
+
+	private float prevTime;
+
+	private void OnValidate ( ) {
+		board = FindObjectOfType<Board>( );
+	}
+
+	private void Update ( ) {
+		// TODO: Make inputs more expandable and better functioning
+		// TODO: Add the ability to hold a button down and have the piece move continuously
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && board.IsMoveInBounds(this, Vector3.left)) {
+			transform.position += Vector3.left;
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow) && board.IsMoveInBounds(this, Vector3.right)) {
+			transform.position += Vector3.right;
+		}
+
+		if (Input.GetKeyDown(KeyCode.UpArrow) && board.IsRotationInBounds(this, -90)) {
+			transform.RotateAround(transform.position, new Vector3(0, 0, 1), -90);
+
+			// TODO: Move piece to the left or right to satisfy a rotation
+		}
+
+		if (Time.time - prevTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime)) {
+			if (board.IsMoveInBounds(this, Vector3.down)) {
+				transform.position += Vector3.down;
+				prevTime = Time.time;
+			} else {
+				board.AddPieceToBoard(this);
+				enabled = false;
+				board.SpawnRandomPiece( );
+			}
+		}
+	}
+}
