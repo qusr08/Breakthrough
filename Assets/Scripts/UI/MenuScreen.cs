17ed83d5b7bb23206b9777e33f8ab0015d48bcf1
qusr08 <34079525+qusr08@users.noreply.github.com>
2023-08-07 17:24:03 -0400

    Started to work on pause menu

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -7,10 +7,12 @@ public class MenuScreen : MonoBehaviour {
 	[SerializeField] private MenuManager menuManager;
 	[SerializeField] private RectTransform rectTransform;
 	[SerializeField] private int _menuLevel;
+	[SerializeField] private Vector2Int _gridDimensions;
 	[SerializeField] private List<GridComponent> _gridComponents;
 
 	#region Properties
 	public int MenuLevel => _menuLevel;
+	public Vector2Int GridDimensions { get => _gridDimensions; private set => _gridDimensions = value; }
 	public List<GridComponent> GridComponents { get => _gridComponents; private set => _gridComponents = value; }
 	#endregion
 
@@ -21,8 +23,7 @@ public class MenuScreen : MonoBehaviour {
 
 		GridComponents = GetComponentsInChildren<GridComponent>( ).ToList( );
 
-		rectTransform.anchorMin = new Vector2(0, 0);
-		rectTransform.anchorMax = new Vector2(1, 1);
+		rectTransform.anchorMin = rectTransform.anchorMax = Vector2.up;
 	}
 
 	private IEnumerator Start ( ) {
@@ -32,14 +33,22 @@ public class MenuScreen : MonoBehaviour {
 
 		OnValidate( );
 
-		// Set the position of the rect transform
-		rectTransform.anchoredPosition = new Vector2(0, -_menuLevel * rectTransform.rect.height);
+		// Clamp the grid dimensions of the menu screen
+		int clampedGridDimensionsX = Mathf.Clamp(GridDimensions.x, 0, menuManager.GridDimensions.x);
+		int clampedGridDimensionsY = Mathf.Clamp(GridDimensions.y, 0, menuManager.GridDimensions.y);
+		GridDimensions = new Vector2Int(clampedGridDimensionsX, clampedGridDimensionsY);
+
+		// Set the position and size of the rect transform
+		rectTransform.sizeDelta = GridDimensions * Constants.UI_GRID_SIZE;
+		float anchoredX = rectTransform.sizeDelta.x / 2f;
+		float anchoredY = (-rectTransform.sizeDelta.y / 2f) + (-MenuLevel * rectTransform.rect.height);
+		rectTransform.anchoredPosition = new Vector2(anchoredX, anchoredY);
 
 		// Generate the grid of blocks in the background based on perlin noise
-		for (int x = 0; x < menuManager.GridDimensions.x; x++) {
-			for (int y = 0; y < menuManager.GridDimensions.y; y++) {
+		for (int x = 0; x < GridDimensions.x; x++) {
+			for (int y = 0; y < GridDimensions.y; y++) {
 				// If the noise value is not equal to 0, then generate a grid box component
-				if (menuManager.GridBoxNoise[x, y + (menuManager.GridDimensions.y * MenuLevel)] >= 0.25) {
+				if (menuManager.GridBoxNoise[x, y + (GridDimensions.y * MenuLevel)] >= 0.25) {
 					menuManager.CreateGridBoxComponent(this, new Vector2Int(x, y));
 				}
 			}
